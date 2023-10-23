17558d6a1c69ff3ab95fd55cb14b0593672b7a15
Eduardo Labian <edulabian@gmail.com>
2023-10-23 13:22:59 +0200

    Agregar archivos de proyecto.

---
+++ PersonWebApp/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace PersonWebApp.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Phone { get; set; }
+        public bool IsActive { get; set; }
+
+        public Person()
+        {
+                
+        }
+    }
+
+    
+}
